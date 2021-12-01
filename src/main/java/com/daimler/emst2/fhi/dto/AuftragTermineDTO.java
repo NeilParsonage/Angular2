fe209ff5fd40c001f41b21481d23ee164c82f511
Reineke, Dominik <dominik.reineke@optiz-consulting.com>
2021-12-01 15:29:01 +0100

    delete row

--- src/main/java/com/daimler/emst2/fhi/dto/AuftragTermineDTO.java
+++ src/main/java/com/daimler/emst2/fhi/dto/AuftragTermineDTO.java
@@ -2,9 +2,6 @@ package com.daimler.emst2.fhi.dto;
 
 import java.util.Date;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class AuftragTermineDTO {
     public String pnr;
 
